242e50f83cd75b08001b60cd0a3f7c6029581d58
Tony Wildish <TonyWildish@users.noreply.github.com>
2024-01-25 11:02:23 +0000

    Update to generate the slug in the correct field, 'rg_slug', and leave the mgmt_resource_group_name field alone

--- devops/scripts/generate-unique-slug.sh
+++ devops/scripts/generate-unique-slug.sh
@@ -16,20 +16,18 @@ set -o nounset
 # exists with the current name, and abort if that's the case.
 here=$(dirname $0)
 config_file="${here}/../../config.yaml"
-mgmt_group=$(yq .management.mgmt_resource_group_name ${config_file})
-if [ "${mgmt_group}" != "__THIS_IS_SET_AUTOMATICALLY__" ]; then
-  exists=$(az group list --query "[?name=='${mgmt_group}'].name" --output tsv)
-  if [ "${exists}" == "${mgmt_group}" ]; then
+rg_slug=$(yq .management.rg_slug ${config_file})
+if [ "${rg_slug}" != "__THIS_IS_SET_AUTOMATICALLY__" ]; then
+  mgmt_group=$(yq .management.mgmt_resource_group_name ${config_file})
+  core_group="rg-${mgmt_group}-${rg_slug}"
+  exists=$(az group list --query "[?name=='${core_group}'].name" --output tsv)
+  if [ "${exists}" == "${core_group}" ]; then
     echo "A previously configured managment group exists with the name \"${mgmt_group}\""
     echo "Delete this group before continuing, or you risk having zombie infrastructure"
     exit 1
   fi
 fi
-printf -v slug "%x" "$(date +%s)"
+printf -v rg_slug "%x" "$(date +%s)"
 
-echo "Updating config file with TRE slug ${slug}"
-yq --inplace \
-  e " \
-      .management.mgmt_resource_group_name = .tre_id + \"-${slug}\" \
-    " \
-    "${config_file}"
+echo "Updating config file with rg_slug ${rg_slug}"
+yq --inplace e ".management.rg_slug = \"${rg_slug}\"" "${config_file}"
